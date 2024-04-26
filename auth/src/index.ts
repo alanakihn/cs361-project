c8b2d25c15827fba6e663b0080b29c304735fe2c
Alana Kihn <alana.kihn@gmail.com>
2024-04-25 18:15:25 -0700

    added postgres database to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,37 @@
 import express, { Express, Request, Response } from "express";
+import { Client } from 'pg';
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const config = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: parseInt(process.env.PGPORT || '5433'),
+};
+
 const app: Express = express();
+const client = new Client(config);
+
+client.connect().then(() => {
+  console.log(`Connected to auth postgres database at ${config.host}:${config.port}`);
+});
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express and TypeScript Server");
+  return res.json({msg: "Express and TypeScript Server"});
+});
+
+app.post("/signup", (req: Request, res: Response) => {
+
+});
+
+app.post("/login", (req: Request, res: Response) => {
+
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
