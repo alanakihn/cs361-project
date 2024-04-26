7db4abb0ee903bd5aa04d4540c47f62d75a99571
Alana Kihn <alana.kihn@gmail.com>
2024-04-26 07:53:44 -0700

    added auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import { Client } from 'pg';
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import Joi, { ObjectSchema } from "joi";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
+
+const validate = (schema: ObjectSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (!error) {
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map((i: Joi.ValidationErrorItem) => i.message).join(",");
+      res.status(400).json({ error: message });
+    }
+  };
+};
 
 const config = {
   user: process.env.PGUSER,
@@ -13,23 +29,95 @@ const config = {
 };
 
 const app: Express = express();
+app.use(express.json());
 const client = new Client(config);
+
+interface User {
+  uid: string,
+  username: string,
+  password: string,
+  created_at: string,
+}
+
+interface ParsedJWT {
+  uid: string,
+  iat: number,
+  exp: number,
+}
 
 client.connect().then(() => {
   console.log(`Connected to auth postgres database at ${config.host}:${config.port}`);
 });
 const port = process.env.PORT || 3000;
+const jwtSecret = process.env.JWT_SECRET!!;
+
+const createNewUser = async (username: string, password: string): Promise<string> => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const uid = await client.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING uid', [username, hashedPassword]);
+
+  return uid.rows[0].uid;
+};
 
 app.get("/", (req: Request, res: Response) => {
   return res.json({msg: "Express and TypeScript Server"});
 });
 
-app.post("/signup", (req: Request, res: Response) => {
+const SignupSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+app.post("/signup", validate(SignupSchema), async (req: Request, res: Response) => {
+  const uid = await createNewUser(req.body.username, req.body.password);
 
+  return res.status(201).json({
+    uid,
+  });
 });
 
-app.post("/login", (req: Request, res: Response) => {
+const LoginSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+app.post("/login", validate(LoginSchema), async (req: Request, res: Response) => {
+  const result = await client.query('SELECT * FROM users WHERE username=$1', [req.body.username]);
 
+  const user: User = result.rows[0];
+
+  if (!user) {
+    return res.status(404).json({
+      error: 'User not found',
+    });
+  }
+  
+  if (await bcrypt.compare(req.body.password, user.password)) {
+    const token = jwt.sign({ uid: user.uid }, jwtSecret, {
+      expiresIn: '1d',
+    });
+    return res.status(200).json({ uid: user.uid, token });
+  }
+
+  return res.status(403).json({
+    error: 'Invalid password',
+  });
+});
+
+const VerifyTokenSchema = Joi.object({
+  uid: Joi.string().required(),
+  token: Joi.string().required(),
+});
+app.post("/verify", validate(VerifyTokenSchema), async (req: Request, res: Response) => {
+  try {
+    const user = jwt.verify(req.body.token, jwtSecret) as ParsedJWT;
+
+    if (user.uid !== req.body.uid) {
+      return res.status(400).json({ error: "Invalid or outdated bearer token" });
+    }
+
+    return res.status(200).send();
+  } catch (err) {
+    return res.status(400).json({ error: "Invalid or outdated bearer token" });
+  }
 });
 
 app.listen(port, () => {
